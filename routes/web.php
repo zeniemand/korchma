92d3ab857846d8c974939c3314e5c3d0f353585a
zeniemand <serhiy_niemand@protonmail.com>
2022-06-12 14:33:06 +0300

    [KM-2] init setup admin controller, view and route with middleware

--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Http\Controllers\Admin\AdminController;
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -20,5 +21,9 @@ Route::get('/', function () {
 Route::get('/dashboard', function () {
     return view('dashboard');
 })->middleware(['auth'])->name('dashboard');
+
+Route::middleware(['auth', 'admin'])->name('admin.')->prefix('admin')->group(function () {
+    Route::get('/', [AdminController::class, 'index'])->name('index');
+});
 
 require __DIR__.'/auth.php';
