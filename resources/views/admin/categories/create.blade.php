3c2e6b71cf97baf362e6b944e8af209ae9e5fc8e
zeniemand <serhiy_niemand@protonmail.com>
2022-06-16 11:46:55 +0300

    [KM-7] prepared create pages

---
+++ resources/views/admin/categories/create.blade.php
@@ -0,0 +1,53 @@
+<x-admin-layout>
+    <x-slot name="header">
+        <h2 class="font-semibold text-xl text-gray-800 leading-tight">
+            {{ __('Dashboard') }}
+        </h2>
+    </x-slot>
+
+    <div class="py-12">
+        <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
+            <div class="flex m-2 p-2">
+                <a href="{{ route('admin.categories.index') }}" class="px-4 py-2 bg-indigo-500 hover:bg-indigo-700 rounded-lg text-white">Back to Category page</a>
+            </div>
+            <div class="m-2 p-2 bg-slate-100 rounded">
+                <div class="space-y-8 divide-y divide-gray-200 w-1/2 mt-10">
+                    <form enctype="multipart/form-data">
+                        <div class="grid gap-6 mb-6 lg:grid-cols-2">
+                            <div class="sm:col-span-6">
+                                <label for="first_name"
+                                       class="block mb-2 text-sm font-medium">Name</label>
+                                <input type="text" id="first_name"
+                                       class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
+                                       placeholder="John" required="">
+                            </div>
+
+                            <div class="sm:col-span-6">
+                                <label for="message"
+                                       class="block mb-2 text-sm font-medium ">Description</label>
+                                <textarea id="message" rows="4"
+                                          class="block p-2.5 w-full text-sm text-gray-900 bg-gray-50 rounded-lg border border-gray-300 focus:ring-blue-500 focus:border-blue-500 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
+                                          placeholder="Leave a comment..."></textarea>
+                            </div>
+
+                            <div class="sm:col-span-6">
+                                <label class="block mb-2 text-sm font-medium "
+                                       for="file_input">Upload image</label>
+                                <input
+                                    class="block w-full text-sm text-gray-900 bg-gray-50 rounded-lg border border-gray-300 cursor-pointer dark:text-gray-400 focus:outline-none dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400"
+                                    id="file_input" type="file">
+                            </div>
+
+                            <div class="sm:col-span-6">
+                                <button type="submit"
+                                        class="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm w-full sm:w-auto px-5 py-2.5 text-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800">
+                                    Store
+                                </button>
+                            </div>
+                        </div>
+                    </form>
+                </div>
+            </div>
+        </div>
+    </div>
+</x-admin-layout>
