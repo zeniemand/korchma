a8a1cc52a6241223a27d61066956b1ba1d9d136c
zeniemand <serhiy_niemand@protonmail.com>
2022-06-19 10:13:06 +0300

    [KM-8] Categories store and display

--- app/Http/Controllers/Admin/CategoryController.php
+++ app/Http/Controllers/Admin/CategoryController.php
@@ -3,6 +3,7 @@
 namespace App\Http\Controllers\Admin;
 
 use App\Http\Controllers\Controller;
+use App\Http\Requests\CategoryStoreRequest;
 use App\Models\Category;
 use Illuminate\Http\Request;
 
@@ -35,9 +36,15 @@ class CategoryController extends Controller
      * @param  \Illuminate\Http\Request  $request
      * @return \Illuminate\Http\Response
      */
-    public function store(Request $request)
+    public function store(CategoryStoreRequest $request)
     {
-        //
+        $image = $request->file('image')->store('public/categories');
+        Category::create([
+            'name' => $request->name,
+            'image' => $image,
+            'description' => $request->description,
+        ]);
+        return to_route('admin.categories.index');
     }
 
     /**
--- resources/views/admin/categories/create.blade.php
+++ resources/views/admin/categories/create.blade.php
@@ -12,30 +12,31 @@
             </div>
             <div class="m-2 p-2 bg-slate-100 rounded">
                 <div class="space-y-8 divide-y divide-gray-200 w-1/2 mt-10">
-                    <form enctype="multipart/form-data">
+                    <form method="POST" action="{{ route('admin.categories.store') }}" enctype="multipart/form-data">
+                        @csrf
                         <div class="grid gap-6 mb-6 lg:grid-cols-2">
                             <div class="sm:col-span-6">
-                                <label for="first_name"
+                                <label for="name"
                                        class="block mb-2 text-sm font-medium">Name</label>
-                                <input type="text" id="first_name"
+                                <input type="text" id="name" name="name"
                                        class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
                                        placeholder="John" required="">
                             </div>
 
                             <div class="sm:col-span-6">
-                                <label for="message"
+                                <label for="description"
                                        class="block mb-2 text-sm font-medium ">Description</label>
-                                <textarea id="message" rows="4"
+                                <textarea id="description" rows="4" name="description"
                                           class="block p-2.5 w-full text-sm text-gray-900 bg-gray-50 rounded-lg border border-gray-300 focus:ring-blue-500 focus:border-blue-500 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
-                                          placeholder="Leave a comment..."></textarea>
+                                          placeholder="Leave a description..."></textarea>
                             </div>
 
                             <div class="sm:col-span-6">
                                 <label class="block mb-2 text-sm font-medium "
-                                       for="file_input">Upload image</label>
+                                       for="image">Upload image</label>
                                 <input
                                     class="block w-full text-sm text-gray-900 bg-gray-50 rounded-lg border border-gray-300 cursor-pointer dark:text-gray-400 focus:outline-none dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400"
-                                    id="file_input" type="file">
+                                    id="image" name="image" type="file">
                             </div>
 
                             <div class="sm:col-span-6">
