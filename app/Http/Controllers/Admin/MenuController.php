7d1e27ec37747bdd23c2e0bdb11d7c13d3bf5304
zeniemand <serhiy_niemand@protonmail.com>
2022-06-22 11:23:07 +0300

    [KM-11] store and display menus

--- app/Http/Controllers/Admin/MenuController.php
+++ app/Http/Controllers/Admin/MenuController.php
@@ -3,6 +3,7 @@
 namespace App\Http\Controllers\Admin;
 
 use App\Http\Controllers\Controller;
+use App\Http\Requests\MenuStoreRequest;
 use App\Models\Category;
 use App\Models\Menu;
 use Illuminate\Http\Request;
@@ -37,9 +38,22 @@ class MenuController extends Controller
      * @param  \Illuminate\Http\Request  $request
      * @return \Illuminate\Http\Response
      */
-    public function store(Request $request)
+    public function store(MenuStoreRequest $request)
     {
-        //
+        $image = $request->file('image')->store('public/menus');
+
+        $menu = Menu::create([
+            'name' => $request->name,
+            'description' => $request->description,
+            'price' => $request->price,
+            'image' => $image
+        ]);
+
+        if ($request->has('categories')) {
+            $menu->categories()->attach($request->categories);
+        }
+
+        return to_route('admin.menus.index');
     }
 
     /**
--- resources/views/admin/menus/create.blade.php
+++ resources/views/admin/menus/create.blade.php
@@ -12,40 +12,63 @@
             </div>
             <div class="m-2 p-2 bg-slate-100 rounded">
                 <div class="space-y-8 divide-y divide-gray-200 w-1/2 mt-10">
-                    <form enctype="multipart/form-data">
+                    <form method="POST" action="{{ route('admin.menus.store') }}" enctype="multipart/form-data">
+                        @csrf
                         <div class="grid gap-6 mb-6 lg:grid-cols-2">
                             <div class="sm:col-span-6">
-                                <label for="first_name"
+                                <label for="name"
                                        class="block mb-2 text-sm font-medium">Name</label>
-                                <input type="text" id="first_name"
+                                <input type="text"
+                                       id="name"
+                                       name="name"
                                        class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
                                        placeholder="John" required="">
                             </div>
 
                             <div class="sm:col-span-6">
-                                <label for="message"
-                                       class="block mb-2 text-sm font-medium ">Description</label>
-                                <textarea id="message" rows="4"
-                                          class="block p-2.5 w-full text-sm text-gray-900 bg-gray-50 rounded-lg border border-gray-300 focus:ring-blue-500 focus:border-blue-500 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
-                                          placeholder="Leave a comment..."></textarea>
+                                <label for="price"
+                                       class="block mb-2 text-sm font-medium">Price</label>
+                                <input type="number"
+                                       id="price"
+                                       name="price"
+                                       min="0.00"
+                                       max="10000.00"
+                                       step="0.01"
+                                       class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
+                                       placeholder="100" required="">
                             </div>
 
                             <div class="sm:col-span-6">
-                                <label for="message"
+                                <label for="description"
+                                       class="block mb-2 text-sm font-medium ">Description</label>
+                                <textarea id="description" rows="4" name="description"
+                                          class="block p-2.5 w-full text-sm text-gray-900 bg-gray-50 rounded-lg border border-gray-300 focus:ring-blue-500 focus:border-blue-500 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
+                                          placeholder="Leave a description..."></textarea>
+                            </div>
+
+                            <div class="sm:col-span-6">
+                                <label for="categories"
                                        class="block mb-2 text-sm font-medium ">Categories</label>
-                                <select multiple>
-                                    @foreach($categories as $categoriy)
-                                        <option>{{ $categoriy->name }}</option>
+                                    <select multiple
+                                        id="categories"
+                                        name="categories[]"
+                                        class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
+                                    >
+                                    @foreach($categories as $category)
+                                        <option value="{{ $category->id }}">{{ $category->name }}</option>
                                     @endforeach
                                 </select>
                             </div>
 
                             <div class="sm:col-span-6">
                                 <label class="block mb-2 text-sm font-medium "
-                                       for="file_input">Upload image</label>
+                                       for="image">Upload image</label>
                                 <input
+                                    id="image"
+                                    name="image"
+                                    type="file"
                                     class="block w-full text-sm text-gray-900 bg-gray-50 rounded-lg border border-gray-300 cursor-pointer dark:text-gray-400 focus:outline-none dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400"
-                                    id="file_input" type="file">
+                                     >
                             </div>
 
                             <div class="sm:col-span-6">
