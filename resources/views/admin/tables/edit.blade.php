d5f9e550a7fcdb35c5e4e2235daea12f48df37d4
zeniemand <serhiy_niemand@protonmail.com>
2022-06-25 12:15:40 +0300

    [KM-14] editing and deleting table

---
+++ resources/views/admin/tables/edit.blade.php
@@ -0,0 +1,81 @@
+<x-admin-layout>
+    <x-slot name="header">
+        <h2 class="font-semibold text-xl text-gray-800 leading-tight">
+            {{ __('Dashboard') }}
+        </h2>
+    </x-slot>
+
+    <div class="py-12">
+        <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
+            <div class="flex m-2 p-2">
+                <a href="{{ route('admin.tables.index') }}" class="px-4 py-2 bg-indigo-500 hover:bg-indigo-700 rounded-lg text-white">Back to Menus page</a>
+            </div>
+            <div class="m-2 p-2 bg-slate-100 rounded">
+                <div class="space-y-8 divide-y divide-gray-200 w-1/2 mt-10">
+                    <form method="POST" action="{{ route('admin.tables.update', $table->id) }}">
+                        @csrf
+                        @method('PUT')
+                        <div class="grid gap-6 mb-6 lg:grid-cols-2">
+                            <div class="sm:col-span-6">
+                                <label for="name"
+                                       class="block mb-2 text-sm font-medium">Name</label>
+                                <input type="text"
+                                       id="name"
+                                       name="name"
+                                       value="{{ $table->name }}"
+                                       class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
+                                       placeholder="John" required="">
+                            </div>
+
+                            <div class="sm:col-span-6">
+                                <label for="price"
+                                       class="block mb-2 text-sm font-medium">Guest</label>
+                                <input type="number"
+                                       id="guest_number"
+                                       name="guest_number"
+                                       value="{{ $table->guest_number }}"
+                                       class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
+                                       placeholder="100" required="">
+                            </div>
+
+                            <div class="sm:col-span-6">
+                                <label for="categories"
+                                       class="block mb-2 text-sm font-medium ">Status</label>
+                                <select
+                                        id="status"
+                                        name="status"
+                                        class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
+                                >
+                                    @foreach(App\Enums\TableStatus::cases() as $status)
+                                        <option value="{{ $status->value }}" @selected($status->value === $table->status->value) >{{ $status->name }}</option>
+                                    @endforeach
+                                </select>
+                            </div>
+
+                            <div class="sm:col-span-6">
+                                <label for="categories"
+                                       class="block mb-2 text-sm font-medium ">Location</label>
+                                    <select
+                                        id="location"
+                                        name="location"
+                                        class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
+                                    >
+                                    @foreach(App\Enums\TableLocation::cases() as $location)
+                                        <option value="{{ $location->value }}" @selected($location->value === $table->location->value) >{{ $location->name }}</option>
+                                    @endforeach
+                                </select>
+                            </div>
+
+                            <div class="sm:col-span-6">
+                                <button type="submit"
+                                        class="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm w-full sm:w-auto px-5 py-2.5 text-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800">
+                                    Update
+                                </button>
+                            </div>
+                        </div>
+                    </form>
+                </div>
+            </div>
+        </div>
+    </div>
+</x-admin-layout>
