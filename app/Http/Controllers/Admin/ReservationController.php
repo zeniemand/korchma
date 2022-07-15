7789c5cab61cde3aacec294e27090d49757b41ef
zeniemand <serhiy_niemand@protonmail.com>
2022-07-15 11:41:00 +0300

    [KM-24] reservation undate and delete

--- app/Http/Controllers/Admin/ReservationController.php
+++ app/Http/Controllers/Admin/ReservationController.php
@@ -77,9 +77,10 @@ class ReservationController extends Controller
      * @param  int  $id
      * @return \Illuminate\Http\Response
      */
-    public function edit($id)
+    public function edit(Reservation $reservation)
     {
-        //
+        $tables = Table::where('status', TableStatus::Available)->get();
+        return view('admin.reservation.edit', compact('reservation', 'tables'));
     }
 
     /**
@@ -89,9 +90,24 @@ class ReservationController extends Controller
      * @param  int  $id
      * @return \Illuminate\Http\Response
      */
-    public function update(Request $request, $id)
+    public function update(ReservationStoreRequest $request, Reservation $reservation)
     {
-        //
+        $table = Table::findOrFail($request->table_id);
+        if ($request->guest_number > $table->guest_number) {
+            return back()->with('warning', 'Please choose the table base on guests number.');
+        }
+
+        $request_date = Carbon::parse($request->res_date);
+        $reservations = $table->reservation()->where('id', '!=', $reservation->id)->get();
+        foreach ($reservations as $reservation) {
+            if($reservation->res_date->format('Y-m-d') == $request_date->format('Y-m-d')){
+                return back()->with('warning', 'This table is reserved for this date');
+            }
+        }
+
+        $reservation->update($request->validated());
+
+        return to_route('admin.reservation.index')->with('success', 'Reservation updated successfully');
     }
 
     /**
@@ -100,8 +116,9 @@ class ReservationController extends Controller
      * @param  int  $id
      * @return \Illuminate\Http\Response
      */
-    public function destroy($id)
+    public function destroy(Reservation $reservation)
     {
-        //
+        $reservation->delete();
+        return to_route('admin.reservation.index')->with('success', 'Reservation deleted successfully');
     }
 }
---
+++ resources/views/admin/reservation/edit.blade.php
@@ -0,0 +1,134 @@
+<x-admin-layout>
+    <x-slot name="header">
+        <h2 class="font-semibold text-xl text-gray-800 leading-tight">
+            {{ __('Dashboard') }}
+        </h2>
+    </x-slot>
+
+    <div class="py-12">
+        <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
+            <div class="flex m-2 p-2">
+                <a href="{{ route('admin.reservation.index') }}"
+                   class="px-4 py-2 bg-indigo-500 hover:bg-indigo-700 rounded-lg text-white">Back to Reservation page</a>
+            </div>
+            <div class="m-2 p-2 bg-slate-100 rounded">
+                <div class="space-y-8 divide-y divide-gray-200 w-1/2 mt-10">
+                    <form method="POST" action="{{ route('admin.reservation.update', $reservation->id) }}">
+                        @csrf
+                        @method('PUT')
+                        <div class="grid gap-6 mb-6 lg:grid-cols-2">
+                            <div class="sm:col-span-6">
+                                <label for="first_name"
+                                       class="block mb-2 text-sm font-medium">First Name</label>
+                                <input type="text"
+                                       id="first_name"
+                                       name="first_name"
+                                       class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
+                                       placeholder="First Name"
+                                       value="{{ old('first_name', $reservation->first_name) }}"
+                                       required="">
+                                @error('first_name')
+                                <div class="text-sm text-red-400">{{ $message }}</div>
+                                @enderror
+                            </div>
+
+                            <div class="sm:col-span-6">
+                                <label for="last_name"
+                                       class="block mb-2 text-sm font-medium">Last Name</label>
+                                <input type="text"
+                                       id="last_name"
+                                       name="last_name"
+                                       class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
+                                       placeholder="Last Name"
+                                       value="{{ old('last_name', $reservation->last_name) }}"
+                                       required="">
+                                @error('last_name')
+                                <div class="text-sm text-red-400">{{ $message }}</div>
+                                @enderror
+                            </div>
+
+                            <div class="sm:col-span-6">
+                                <label for="email"
+                                       class="block mb-2 text-sm font-medium">Email</label>
+                                <input type="email"
+                                       id="email"
+                                       name="email"
+                                       class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
+                                       value="{{ old('email', $reservation->email) }}"
+                                       required="">
+                                @error('email')
+                                <div class="text-sm text-red-400">{{ $message }}</div>
+                                @enderror
+                            </div>
+
+                            <div class="sm:col-span-6">
+                                <label for="phone_number"
+                                       class="block mb-2 text-sm font-medium">Phone Number</label>
+                                <input type="text"
+                                       id="phone_number"
+                                       name="phone_number"
+                                       class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
+                                       value="{{ old('phone_number', $reservation->phone_number) }}"
+                                       required="">
+                                @error('phone_number')
+                                <div class="text-sm text-red-400">{{ $message }}</div>
+                                @enderror
+                            </div>
+
+                            <div class="sm:col-span-6">
+                                <label for="res_date"
+                                       class="block mb-2 text-sm font-medium">Reservation Date</label>
+                                <input type="datetime-local"
+                                       id="res_date"
+                                       name="res_date"
+                                       class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
+                                       value="{{ old('res_date', $reservation->res_date->format('Y-m-d\TH:i:s')) }}"
+                                       required="">
+                                @error('res_date')
+                                <div class="text-sm text-red-400">{{ $message }}</div>
+                                @enderror
+                            </div>
+
+                            <div class="sm:col-span-6">
+                                <label for="guest_number"
+                                       class="block mb-2 text-sm font-medium">Guest number</label>
+                                <input type="number"
+                                       id="guest_number"
+                                       name="guest_number"
+                                       class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
+                                       value="{{ old('guest_number', $reservation->guest_number) }}"
+                                       required="">
+                                @error('guest_number')
+                                <div class="text-sm text-red-400">{{ $message }}</div>
+                                @enderror
+                            </div>
+
+                            <div class="sm:col-span-6">
+                                <label for="table_id"
+                                       class="block mb-2 text-sm font-medium ">Table</label>
+                                <select id="table_id"
+                                        name="table_id"
+                                        class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
+                                >
+                                    @foreach($tables as $table)
+                                        <option value="{{ $table->id }}" @selected(old('table_id', $reservation->table_id) == $table->id)>{{ $table->name }} ( for {{ $table->guest_number }} guests ) </option>
+                                    @endforeach
+                                </select>
+                                @error('table_id')
+                                <div class="text-sm text-red-400">{{ $message }}</div>
+                                @enderror
+                            </div>
+
+                            <div class="sm:col-span-6">
+                                <button type="submit"
+                                        class="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm w-full sm:w-auto px-5 py-2.5 text-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800">
+                                    Update
+                                </button>
+                            </div>
+                        </div>
+                    </form>
+                </div>
+            </div>
+        </div>
+    </div>
+</x-admin-layout>
